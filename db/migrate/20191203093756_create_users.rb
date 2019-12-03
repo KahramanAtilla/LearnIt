e9fe027c1bc51bd30a79f3a8e16c6c843bb86d82
Atilla <atillafacebook@hotmail.fr>
2019-12-03 10:57:28 +0100

    mailer and user done

---
+++ db/migrate/20191203093756_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :city
+      t.string :school
+      t.string :diploma
+
+      t.timestamps
+    end
+  end
+end
