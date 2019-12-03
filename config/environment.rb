b4e751d5d655542c5d1e9ca62818f5860d39e18a
Atilla <atillafacebook@hotmail.fr>
2019-12-03 11:10:21 +0100

    mailer corrected

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
