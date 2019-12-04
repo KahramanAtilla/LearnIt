99a6804a3857f95f41c4c47572d995888c035816
Atilla <atillafacebook@hotmail.fr>
2019-12-04 09:56:54 +0100

    favorite table creating but without links

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_144509) do
+ActiveRecord::Schema.define(version: 2019_12_03_150248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.string "title"
