2b2c27949b704bc25f8405ec0e00a419db20408c
nohelathp <nohela2000@gmail.com>
2019-12-04 11:39:44 +0100

    controller + view show topic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'topics/show'
   get 'kit_ui/index'
   devise_for :users
   resources :lessons
+  resources :topics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
