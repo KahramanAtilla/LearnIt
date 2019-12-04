5b8813c3108be86fc826275d0345f95cd44345d7
nohelathp <nohela2000@gmail.com>
2019-12-04 22:18:10 +0100

    modif controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,5 +6,10 @@ class LessonsController < ApplicationController
   def show 
   	@lesson = Lesson.find(params[:id])
   end
+  
+  def new
+  	@lesson = Lesson.new(user: current_user)
+  	@topics = Topic.all
+  end 
 
 end
