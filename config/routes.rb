a4985eaed02629f41205d6b5db6b5e7affc39b48
anderoma <antoine.deroma@gmail.com>
2019-12-13 11:37:22 +0100

    footer_view

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
   get 'topics/show'
-  get 'kit_ui/index'
   root 'lessons#index'
   devise_for :users
   resources :lessons
   resources :topics
   resources :comments
   resources :favorites
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
